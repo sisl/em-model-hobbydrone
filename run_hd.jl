f4771c09a056a5d1b5d9ac48012492f069d9af2c
Sydney Katz <sydneykatz@Sydneys-MacBook-Pro-5.local>
2020-03-25 10:20:22 -0700

    sample run script

---
+++ run_hd.jl
@@ -0,0 +1,8 @@
+# Basic test script - generates the file sample_hd_traj.csv containing one hobby drone trajectory
+
+# Include the files
+include("HobbyDroneInterface.jl")
+# Set the random seed
+Random.seed!(1)
+# Generate the trajectory file
+generate_trajectory_file(1.0, "sample_hd_traj.csv")
